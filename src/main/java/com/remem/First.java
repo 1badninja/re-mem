e13d39a68dec5b7e8439e2f0d8b2253265827d25
1badninja <1badninja@gmail.com>
2012-02-27 01:30:56 -0600

    First java commit

---
+++ src/main/java/com/remem/First.java
@@ -0,0 +1,18 @@
+package main.java.com.remem;
+
+import java.util.Arrays;
+
+/**
+ * TODO: Describe me!
+ * <p/>
+ * User: rmarquez
+ * Date: 2/27/12
+ * Time: 1:29 AM
+ */
+public class First
+{
+    public static void main (String[] args)
+    {
+        System.out.println("args: " + Arrays.toString(args));
+    }
+}
